9506ecb530ecbd8e77392115814468feaf77441e
Tim Tuvestam <timtuvestam@hotmail.se>
2018-12-17 22:16:15 +0100

    Snow

---
+++ EventTest/EventTest/Game1.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.Collections.Generic;
+
+namespace EventTest
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        delegate void Updates();
+        Updates updateHandler;
+        event Updates UpdateEvent;
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        List<Things> thingList = new List<Things>();
+        public static Texture2D tex;
+        int i = 0;
+        Song canon;
+        Texture2D background;
+
+        
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferHeight = 1080;
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.IsFullScreen = true;
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            tex = Content.Load<Texture2D>("Flakes1");
+            canon = Content.Load<Song>("Canon");
+            MediaPlayer.Play(canon);
+            MediaPlayer.IsRepeating = true;
+            background = Content.Load<Texture2D>("Winter");
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            if(updateHandler != null)
+            updateHandler.Invoke();
+
+           
+            // TODO: Add your update logic here
+            for (int i = 0; i < thingList.Count; i++)
+            {
+                if(thingList[i].position.Y >graphics.PreferredBackBufferHeight)
+                {
+                    updateHandler -= thingList[i].Update;
+                    thingList.RemoveAt(i);
+                    i--;
+                }
+
+            }
+            CreateSnow();
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin(SpriteSortMode.Deferred,BlendState.Additive);
+            spriteBatch.Draw(background, new Rectangle(0, 0, 1920, 1080), Color.White);
+            foreach (var item in thingList)
+            {
+                item.Draw(spriteBatch);
+            }
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+
+        void CreateTexture()
+        {
+            Color[] data = new Color[100];
+
+            for (int i = 0; i < 100; i++)
+            {
+                data[i] = Color.White;
+            }
+            tex = new Texture2D(GraphicsDevice, 10, 10);
+
+            tex.SetData(data);
+        }
+
+        void CreateSnow()
+        {
+            i++;
+            if (i % 1 == 0)
+            {
+                Things t = new Things();
+                thingList.Add(t);
+                updateHandler += t.Update;
+            }
+        }
+
+
+    }
+}
---
+++ EventTest/EventTest/Things.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTest
+{
+    class Things
+    {
+        static Random rand = new Random();
+        Texture2D texture;
+        public Vector2 position;
+        float speed;
+        float radius;
+        Vector2 velocity;
+        float gravity = 0.0001f;
+        float xOffset = 0;
+        float angle;
+        int imageX;
+        int imageY;
+        
+
+        public Things()
+        {
+            texture = Game1.tex;
+            position = new Vector2(rand.Next(0, 1920), rand.Next(-100,-10));
+            radius = GetSize();
+            angle = RandomFloat(0, MathHelper.TwoPi);
+            imageX = rand.Next(16);
+            imageY = rand.Next(16);
+
+        }
+
+        public void Update()
+        {
+            xOffset = (float)Math.Sin(angle) * radius/100;
+            velocity.Y += gravity  * radius;
+            velocity.X = xOffset;
+            if (velocity.Y > radius * 0.2f)
+                velocity.Y = radius * 0.2f;
+            position += velocity;
+            angle += (float)Math.Sqrt(velocity.X * velocity.X + velocity.Y * velocity.Y)/1000;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture,new Rectangle(position.ToPoint(),new Point((int)radius,(int)radius)),new Rectangle(imageX*32,imageY*32,32,32), Color.White,angle,new Vector2(16,16), SpriteEffects.None,0);
+        }
+
+        public void ApplyForce(Vector2 force)
+        {
+            velocity += force;
+        }
+
+        int GetSize()
+        {
+            while (true)
+            {
+                int r1 = rand.Next(1,40);
+                int r2 = rand.Next(1,40);
+                if (r2 > r1)
+                    return r1;
+            }
+        }
+        float RandomFloat(float min, float max)
+        {
+            return (float)rand.NextDouble() * (max - min) + min;
+        }
+    }
+}
